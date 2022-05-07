db0a9ab7c17ef707cc6379830845d6d1a9ee114c
Haris Ali <haris.aali9211@gmail.com>
2022-05-07 20:39:44 +0500

    getting coordinate

--- gps-sender/App.js
+++ gps-sender/App.js
@@ -1,13 +1,76 @@
+import React, { useState, useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import MainComponent from './Component/MainComonent'
+import { StyleSheet, Text, View, Button } from 'react-native';
+import * as Location from "expo-location";
+import * as Application from "expo-application";
 
 export default function App() {
+
+  const [errorMsg, setErrorMsg] = React.useState(null);
+  const [isTracking, setIsTracking] = React.useState(false);
+  const [permission, setPermission] = React.useState(false);
+  const [location, setLocation] = useState(null);
+
+
+  const getLocation = async () => {
+    if (!permission) {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        setErrorMsg("Permission to access location was denied");
+        setIsTracking(false);
+        return;
+      }
+      setPermission(true);
+      setErrorMsg("");
+    } else setIsTracking(true);
+    let location = await Location.getCurrentPositionAsync({});
+    setLocation(location);
+  };
+
+  const handleClick = () => {
+    setErrorMsg("");
+    setIsTracking(true);
+  };
+
+  const stopTracking = () => {
+    setIsTracking(false);
+    setLocation(null);
+    setErrorMsg("");
+  };
+
+  React.useEffect(() => {
+    let int = null;
+    if (isTracking) {
+      int = setInterval(() => {
+        getLocation();
+      }, 1000);
+    } else {
+      clearInterval(int);
+    }
+    return () => {
+      clearInterval(int);
+    };
+  }, [isTracking]);
+
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Text>hello may name is haris ali</Text>
-      <Text>kia karty hooo</Text>
+
+      <Text
+        style={styles.titleText}
+      >Latitude: {location?.coords.latitude}
+      </Text>
+      <Text
+        style={styles.titleText}
+      >Longitude: {location?.coords.longitude}</Text>
+      <Text style={{ color: "red" }}>{errorMsg}</Text>
+
+      <Button
+        color={isTracking ? "#C70039" : "#138D75"}
+        title={isTracking ? "Stop Tracking" : "Start Tracking"}
+        onPress={isTracking ? stopTracking : handleClick}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -20,4 +83,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  titleText: {
+    fontSize: 20,
+    fontWeight: "bold"
+  }
+
 });
