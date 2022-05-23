43a411402a1bedf10eeaf2fad9ff59bef1e87089
Haris Ali <64413337+haris-ali9211@users.noreply.github.com>
2022-05-23 14:54:52 +0500

    socket-io

---
+++ gps-sender/Backend/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+
+
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:19002",
+        methods: ["GET", "POst"]
+    },
+});
+
+server.listen(3001, () => {
+    console.log("🚀 ~ Server is listening");
+});
