c1c06b3a3491ab623caf5cf80f25da9818fb4dce
Haris Ali <64413337+haris-ali9211@users.noreply.github.com>
2022-05-23 16:08:10 +0500

    some socket-io update

--- gps-sender/Backend/index.js
+++ gps-sender/Backend/index.js
@@ -11,11 +11,14 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:19002",
+        origin: "http://localhost:19002/",
         methods: ["GET", "POst"]
     },
 });
 
+io.on("connection", (socket)=>{
+    console.log(`User Connected ${socket.id}`)
+})
 server.listen(3001, () => {
     console.log("🚀 ~ Server is listening");
 });
--- gps-sender/Client/App.js
+++ gps-sender/Client/App.js
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 import * as Location from "expo-location";
 import * as Application from "expo-application";
+import io  from 'socket.io-client';
+const socket = io.connect("http://localhost:3001"); 
 
 export default function App() {
 
@@ -19,7 +21,7 @@ export default function App() {
         setErrorMsg("Permission to access location was denied");
         setIsTracking(false);
         return;
-      }       
+      }
       setPermission(true);
       setErrorMsg("");
     } else setIsTracking(true);
@@ -52,6 +54,9 @@ export default function App() {
     };
   }, [isTracking]);
 
+  const sentMenages = () => {
+
+  }
 
 
   return (
@@ -71,6 +76,18 @@ export default function App() {
         title={isTracking ? "Stop Tracking" : "Start Tracking"}
         onPress={isTracking ? stopTracking : handleClick}
       />
+
+      <View>
+        <TextInput
+          placeholder='hello'
+          style={styles.input}
+        />
+        <Button
+          title='click'
+          onPress={sentMenages}
+          color={"#138D75"}
+        />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -86,6 +103,15 @@ const styles = StyleSheet.create({
   titleText: {
     fontSize: 20,
     fontWeight: "bold"
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    width: 200,
+    marginHorizontal: 22,
+    borderRadius: 12
   }
 
 });
