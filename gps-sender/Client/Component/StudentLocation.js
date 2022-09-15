98a956b63e14664e4e498ce665f4dd7a60818e1d
Haris Ali <64413337+haris-ali9211@users.noreply.github.com>
2022-09-16 03:30:15 +0500

    direction done

--- gps-sender/Client/Component/StudentLocation.js
+++ gps-sender/Client/Component/StudentLocation.js
@@ -1,59 +1,184 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
 
 
-import MapView from 'react-native-maps';
-import { Marker, Circle } from 'react-native-maps';
+import MapView, { Marker, Circle, Polyline } from 'react-native-maps';
+import MapViewDirections from 'react-native-maps-directions';
+
+
 
 import { ref, set, get, child } from "firebase/database"
 import firebaseStack from '../firebase/Firebase';
 
+import * as Location from "expo-location";
+import getDirections from '../Function/function';
 
+
+
+//! style for map
+
+const mapStyle = [
+    {
+        "elementType": "geometry.fill",
+        "stylers": [
+            { "visibility": "on" },
+            { "color": "#324447" }
+        ]
+    }, {
+        "elementType": "geometry.stroke",
+        "stylers": [
+            { "visibility": "on" },
+            { "color": "#263538" }
+        ]
+    }, {
+        "featureType": "landscape",
+        "elementType": "geometry.fill",
+        "stylers": [
+            { "visibility": "on" },
+            { "color": "#324447" }
+        ]
+    }, {
+        "featureType": "administrative",
+        "elementType": "geometry.stroke",
+        "stylers": [
+            { "color": "#152022" },
+            { "visibility": "on" },
+            { "weight": 1 }
+        ]
+    }, {
+        "featureType": "water",
+        "elementType": "geometry.fill",
+        "stylers": [
+            { "visibility": "on" },
+            { "color": "#2d4a5a" }
+        ]
+    }, {
+        "elementType": "labels.text.fill",
+        "stylers": [
+            { "visibility": "on" },
+            { "color": "#cccccc" }
+        ]
+    }, {
+        "elementType": "labels.text.stroke",
+        "stylers": [
+            { "visibility": "on" },
+            { "weight": 0.1 },
+            { "color": "#152022" }
+        ]
+    }, {
+        "featureType": "poi",
+        "elementType": "geometry",
+        "stylers": [
+            { "visibility": "on" },
+            { "color": "#473d40" }
+        ]
+    }, {
+        "featureType": "poi.park",
+        "elementType": "geometry.fill",
+        "stylers": [
+            { "visibility": "on" },
+            { "color": "#2f4e43" }
+        ]
+    }, {
+        "featureType": "transit.station.airport",
+        "elementType": "geometry.fill",
+        "stylers": [
+            { "visibility": "on" },
+            { "color": "#473d40" }
+        ]
+    }, {
+        "featureType": "poi",
+        "elementType": "labels.text",
+        "stylers": [
+            { "visibility": "simplified" },
+            { "lightness": -24 }
+        ]
+    }, {
+        "featureType": "road",
+        "elementType": "geometry.fill",
+        "stylers": [
+            { "visibility": "on" },
+            { "color": "#263538" }
+        ]
+    }, {
+        "featureType": "road",
+        "elementType": "geometry.stroke",
+        "stylers": [
+            { "visibility": "off" }
+        ]
+    }, {
+        "featureType": "transit.line",
+        "elementType": "geometry.fill",
+        "stylers": [
+            { "visibility": "on" },
+            { "color": "#243942" }
+        ]
+    }, {
+        "featureType": "transit.line",
+        "elementType": "geometry.stroke",
+        "stylers": [
+            { "visibility": "on" },
+            { "color": "#263538" }
+        ]
+    }
+]
+
+const origin = { latitude: 24.9547844, longitude: 67.067563 };
+const destination = { latitude: 24.9283654, longitude: 67.0755405 };
+const GOOGLE_MAPS_APIKEY = 'AIzaSyBpQa3iZZhr1no2ymxxhgBGrck7rlhO1gg';
 
 export default function App() {
 
-    const defaultOptions = {
-        strokeOpacity: 0.5,
-        strokeWeight: 2,
-        clickable: false,
-        draggable: false,
-        editable: false,
-        visible: true,
-    };
-    const closeOptions = {
-        ...defaultOptions,
-        zIndex: 3,
-        fillOpacity: 0.05,
-        strokeColor: "#8BC34A",
-        fillColor: "#8BC34A",
-    };
-    const middleOptions = {
-        ...defaultOptions,
-        zIndex: 2,
-        fillOpacity: 0.05,
-        strokeColor: "#FBC02D",
-        fillColor: "#FBC02D",
-    };
-    const farOptions = {
-        ...defaultOptions,
-        zIndex: 1,
-        fillOpacity: 0.05,
-        strokeColor: "#FF5252",
-        fillColor: "#FF5252",
-    };
+    const [currentLocation, setCurrentLocation] = useState(null);
+    const [errorMsg, setErrorMsg] = React.useState(null);
+    const [isTracking, setIsTracking] = React.useState(true);
+    const [permission, setPermission] = React.useState(false);
+    const [location, setLocation] = useState(null);
+    const [LocationOfDriver, setLocationOfDriver] = useState([]);
 
+
+
+    const getLocation = async () => {
+        if (!permission) {
+            let { status } = await Location.requestForegroundPermissionsAsync();
+            if (status !== "granted") {
+                setErrorMsg("Permission to access location was denied");
+                setIsTracking(false);
+                return;
+            }
+            setPermission(true);
+            setErrorMsg("");
+        } else setIsTracking(true);
+        let location = await Location.getCurrentPositionAsync({});
+        setLocation(location);
+    }
+
+    React.useEffect(() => {
+        let int = null;
+        if (isTracking) {
+            int = setInterval(() => {
+                getLocation();
+            }, 10000);
+        } else {
+            clearInterval(int);
+        }
+        return () => {
+            clearInterval(int);
+        };
+    }, [isTracking]);
+
+
+    //! firebase setup
 
     const db = firebaseStack();
     const dbRef = ref(firebaseStack());
 
-    const [location, setLocation] = useState([]);
-
-
+    //! function calling data for current location
 
     const getLocationOfDriver = async () => {
         get(child(dbRef, `Location/`)).then((snapshot) => {
             if (snapshot.exists()) {
-                setLocation(snapshot.val())
+                setLocationOfDriver(snapshot.val())
             } else {
                 console.log("No data available");
             }
@@ -63,10 +188,22 @@ export default function App() {
     }
 
 
-    useEffect(() => {
-        getLocationOfDriver()
-    }, [])
+    // React.useEffect(() => {
+    //     let int = null;
+    //     if (isTracking) {
+    //       int = setInterval(() => {
+    //         getLocationOfDriver();
+    //       }, 10000);
+    //     } else {
+    //       clearInterval(int);
+    //     }
+    //     return () => {
+    //       clearInterval(int);
+    //     };
+    //   }, [isTracking]);
 
+
+    //! hardcode location  
 
     const [region, setRegion] = useState({
         latitude: 51.5079145,
@@ -83,37 +220,100 @@ export default function App() {
     };
 
     const tokyoRegion1 = {
-        latitude: 35.6762,
-        longitude: 139.6503,
+        latitude: location ? location.coords.latitude : 24.8687345,
+        longitude: location ? location.coords.longitude : 67.0677422,
         latitudeDelta: 0.01,
         longitudeDelta: 0.01,
     };
+
+    const mapRef = useRef(null);
+
+    const goToTokyo = () => {
+        //complete this animation in 3 seconds
+        mapRef.current.animateToRegion(tokyoRegion1, 3 * 1000);
+    };
+
     return (
         <View style={styles.container}>
             <MapView
+                ref={mapRef}
                 style={styles.map}
                 initialRegion={tokyoRegion}
                 onRegionChangeComplete={(region) => setRegion(region)}
-                // customMapStyle={ab486c4a82f56e5d}
+            // customMapStyle={mapStyle}
             >
-                <Marker coordinate={tokyoRegion} />
-                {/* <Marker
-                    coordinate={region}
-                    pinColor="green"
-                // image={require("../image/bean.png")}
+                {/* <Marker coordinate={tokyoRegion} /> */}
+
+                {
+                    location != null && mapRef != null
+                        ?
+                        <View>
+                            <Marker
+                                coordinate={tokyoRegion1}
+                            // image={require("../image/bean.png")}
+                            // width={8}
+                            // height={8}
+                            >
+                                <Image source={require("../image/deadpool.png")} style={{ height: 35, width: 35 }} />
+
+                            </Marker>
+
+                            <Circle
+                                zIndex={3}
+                                center={tokyoRegion1}
+                                radius={800}
+                                strokeWidth={1}
+                                strokeColor={'#8BC34A'}
+                                fillColor={'rgba(139,195,74,0.2)'} />
+                            <Circle
+                                zIndex={2}
+                                center={tokyoRegion1}
+                                radius={2700}
+                                strokeWidth={1}
+                                strokeColor={'#FFFF00'}
+                                fillColor={'rgba(255,255,0,0.2)'} />
+                            <Circle
+                                zIndex={0}
+                                center={tokyoRegion1}
+                                radius={4200}
+                                strokeWidth={1}
+                                strokeColor={'#FF5252'}
+                                fillColor={'rgba(255,82,82,0.2)'} />
+                        </View>
+
+
+                        :
+                        null
+
+                }
+                {/* <MapViewDirections
+                    origin={origin}
+                    destination={destination}
+                    apikey={GOOGLE_MAPS_APIKEY}
+                    strokeWidth={3}
+                    strokeColor="hotpink"
                 /> */}
-                {/* <Circle center={region} radius={1500} options={closeOptions} />
-                <Circle center={region} radius={3000} options={middleOptions} />
-                <Circle center={region} radius={4500} options={farOptions} /> */}
+
             </MapView>
             <Text style={styles.text}>Current latitude: {region.latitude}</Text>
             <Text style={styles.text}>Current longitude: {region.longitude}</Text>
 
-            <TouchableOpacity>
-                <View>
-                    <Text style={{ borderBottomWidth: 1, borderColor: "grey", marginVertical: 10, backgroundColor:'red' }}>Current Location</Text>
-                </View>
-            </TouchableOpacity>
+            {
+                location != null ? <Text>location hai</Text> : <Text>location nahi hai</Text>
+            }
+
+            {
+                location != null
+                    ?
+                    <TouchableOpacity onPress={goToTokyo}>
+                        <View>
+                            <Text style={{ borderBottomWidth: 1, borderColor: "grey", marginVertical: 10, backgroundColor: 'red' }}>Current Location</Text>
+                        </View>
+                    </TouchableOpacity>
+                    :
+                    null
+            }
+
         </View>
     );
 }
---
+++ gps-sender/Client/Function/function.js
@@ -0,0 +1,25 @@
+import { decode } from "@mapbox/polyline"; 
+
+const getDirections = async (startLoc, destinationLoc) => {
+    try {
+        const KEY = "AIzaSyBpQa3iZZhr1no2ymxxhgBGrck7rlhO1gg"; //put your API key here.
+        //otherwise, you'll have an 'unauthorized' error.
+        let resp = await fetch(
+            `https://maps.googleapis.com/maps/api/directions/json?origin=${startLoc}&destination=${destinationLoc}&key=${KEY}`
+        );
+        let respJson = await resp.json();
+        let points = decode(respJson.routes[0].overview_polyline.points);
+        console.log(points);
+        let coords = points.map((point, index) => {
+            return {
+                latitude: point[0],
+                longitude: point[1]
+            };
+        });
+        return coords;
+    } catch (error) {
+        return error;
+    }
+};
+
+export default getDirections
